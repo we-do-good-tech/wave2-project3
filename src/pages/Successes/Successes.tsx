19ccc8bc3b76ed05680a653c945890fad95ac043
Snir Shushan <snirshu@Snirs-MacBook-Pro.local>
2021-03-03 00:57:48 +0200

    update-3/3

--- src/pages/Successes/Successes.tsx
+++ src/pages/Successes/Successes.tsx
@@ -3,26 +3,55 @@ import styles from './styles.module.scss';
 import classnames from 'classnames';
 import { ISuccesses, ISuccessesCategory } from './consts';
 import Caroline from '../../images/tips/Carolin.png';
+import { Carousel, Button, Modal } from 'react-bootstrap';
 
 export const Successes: FC = () => {
   const [successebox, setSuccesseBox] = useState<ISuccessesCategory>(ISuccesses[0]);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className={styles.container}>
       <div className={classnames(styles.Column1)}>
         {ISuccesses.map((ISuccesse: ISuccessesCategory) => (
-          <div
-            key={ISuccesse.id}
-            className={classnames(styles.cards)}
-            style={{ border: '8px solid', borderColor: ISuccesse.color }}
-            onClick={() => setSuccesseBox(ISuccesse)}>
-            <div className={classnames(styles.card)} style={{ backgroundColor: ISuccesse.color }}>
-              <img loading='lazy' src={Caroline} />
-              <h1>ההצלחה של ישראלה ישראלי</h1>
-              <h2>ישראלה היא ספורטאית פראלימפית שבשבילה השמיים הם לא הגבול!</h2>
-              <button style={{ color: ISuccesse.color }}>כנסו לקרוא</button>
-            </div>
-          </div>
+          <Carousel>
+            <Carousel.Item>
+              <div
+                key={ISuccesse.id}
+                className={classnames(styles.cards)}
+                style={{ border: '8px solid', borderColor: ISuccesse.color }}
+                onClick={() => setSuccesseBox(ISuccesse)}>
+                <div className={classnames(styles.card)} style={{ backgroundColor: ISuccesse.color }}>
+                  <img loading='lazy' src={Caroline} />
+
+                  <Carousel.Caption>
+                    <h1>ההצלחה של {ISuccesse.name}</h1>
+                    <h2>{ISuccesse.detail}</h2>
+                    <Button onClick={handleShow} style={{ color: ISuccesse.color }}>
+                      כנסו לקרוא
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose} style={{ backgroundColor: ISuccesse.color }}>
+                      <Modal.Header>
+                        <Modal.Title>ההצלחה של {ISuccesse.name}</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <h1>{ISuccesse.question}</h1>
+                        <h2>{ISuccesse.answer}</h2>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant='secondary' onClick={handleClose}>
+                          סגירה
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </Carousel.Caption>
+                </div>
+              </div>
+            </Carousel.Item>
+          </Carousel>
         ))}
       </div>
     </div>
--- src/pages/Successes/consts.ts
+++ src/pages/Successes/consts.ts
@@ -4,35 +4,45 @@ export interface ISuccessesCategory {
   detail: string;
   image?: String;
   color: string;
+  question: string;
+  answer: string;
 }
 
 export const ISuccesses: ISuccessesCategory[] = [
   {
     id: 1,
-    name: 'ליאת',
-    detail: 'בת',
+    name: 'שם ומשפחה',
+    detail: 'סיפור הצלחה',
     image: '',
     color: '#4fb27f',
+    question: 'שאלה 1',
+    answer: 'תשובה 1',
   },
   {
     id: 2,
-    name: 'ליאת',
-    detail: 'בת',
+    name: 'שם ומשפחה 2',
+    detail: 'סיפור הצלחה 2',
     image: '',
     color: '#f18267',
+    question: 'שאלה 2',
+    answer: 'תשובה 2',
   },
   {
     id: 3,
-    name: 'ליאת',
-    detail: 'בת',
+    name: 'שם ומשפחה 3',
+    detail: 'סיפור הצלחה 3',
     image: '',
     color: '#60cce8',
+    question: 'שאלה 3',
+    answer: 'תשובה 3',
   },
   {
     id: 4,
-    name: 'ליאת',
-    detail: 'בת',
+    name: 'שם ומשפחה 4',
+    detail: 'סיפור הצלחה 4',
     image: '',
     color: '#4fb27f',
+    question: 'שאלה 4',
+    answer: 'תשובה 4',
   },
 ];
