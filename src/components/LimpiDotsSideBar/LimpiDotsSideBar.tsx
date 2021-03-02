87c87a1e4fc7de5e1b9239a0088911046e3e9405
tomerk97 <tomerk1997@gmail.com>
2021-03-03 01:06:03 +0200

    alt nav

--- src/components/LimpiDotsSideBar/LimpiDotsSideBar.tsx
+++ src/components/LimpiDotsSideBar/LimpiDotsSideBar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import styles from './styles.module.scss';
 import classnames from 'classnames';
 import { limpiHomeComponents } from '../../pages/LimpiHome/consts';
@@ -8,7 +8,21 @@ import { jumpTo } from '../../utils/jumpTo';
 
 export const LimpiDotsSideBar: FC = () => {
   const { y: scrollTopPosition } = useScrollPosition();
-  const customerWindowHeight = window.innerHeight;
+
+  const { scrollHeight, scrollTop } = document.scrollingElement ?? { scrollHeight: 1, scrollTop: 0 };
+
+  const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  const percentage = useMemo(() => (scrollTop / winHeightPx) * 100, [scrollHeight, scrollTop]);
+
+  const sideBarClick = useCallback((e) => {
+    const clickPercentage = e.clientY / window.screen.availHeight;
+    const { clientHeight } = document.scrollingElement || { clientHeight: 0 };
+    console.log(clickPercentage);
+    window.scrollTo({ left: 0, top: document.documentElement.scrollHeight * clickPercentage, behavior: 'smooth' });
+  }, []);
+
+  const customerWindowHeight = window.screen.availHeight;
   const currentActiveIdx =
     toInteger(scrollTopPosition / (customerWindowHeight * 0.8)) < limpiHomeComponents.length
       ? toInteger(scrollTopPosition / (customerWindowHeight * 0.8))
@@ -23,7 +37,7 @@ export const LimpiDotsSideBar: FC = () => {
 
   return (
     <div className={styles.sideBarContainer}>
-      <div className={styles.sideBar}>{renderedDots}</div>
+      <div style={{ backgroundSize: `100% ${percentage}%` }} className={styles.container} onClick={sideBarClick}></div>
     </div>
   );
 };
