798c6e6bd8708f58065e6038f143c98f45da0a4c
tomerk97 <tomerk1997@gmail.com>
2021-03-05 15:52:01 +0200

    responsive changes

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -6,7 +6,6 @@ import styles from './styles.module.scss';
 import logo from '../../images/logoSmall.svg';
 import { useScrollPosition } from 'react-use-scroll-position';
 import classnames from 'classnames';
-import { Loading } from '../../components/Loading';
 
 export const About: FC = () => {
   const { y: scrollTopPosition } = useScrollPosition();
@@ -21,8 +20,7 @@ export const About: FC = () => {
         className={classnames('limpiLogo', scrollTopPosition >= 40 && 'limpiLogoRight')}
       />
       <h1>זה המקום בו תוכלו לחלום ולהבין שלפעמים דברים גדולים מתחילים בהחלטה קטנה וההחלטה הזו היא שלכם</h1>
-      {!isLoaded && <Loading />}
-      <img loading='eager' onLoad={() => setLoaded()} className={styles.mainImage} alt='logo' src={main} />
+      <img loading='eager' className={styles.mainImage} alt='logo' src={main} />
       <LimpiArrowRoute toPage={880} />
     </div>
   );
