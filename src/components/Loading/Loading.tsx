d589e130a146fd45e795c55f7cad25320bc71a86
tomerk97 <tomerk1997@gmail.com>
2021-03-17 21:21:23 +0200

    fix bug

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,12 @@
+import { useMount, useUnmount } from 'ahooks';
 import React, { FC } from 'react';
+import { lockScreen, unlockScreen } from '../../utils/lockScreen';
 import styles from './styles.module.scss';
 
 export const Loading: FC = () => {
+  useMount(() => lockScreen());
+  useUnmount(() => unlockScreen());
+
   return (
     <div className={styles.loading}>
       <label className={styles.loader} />
