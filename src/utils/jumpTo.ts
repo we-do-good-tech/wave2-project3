9752ae369b5afd90ae5c7b34f6c4beb26bb261a1
tomerk97 <tomerk1997@gmail.com>
2021-03-30 16:42:01 +0300

    first page fix, scroll fix, arrow route fix, logo fix

--- src/utils/jumpTo.ts
+++ src/utils/jumpTo.ts
@@ -1,8 +1,8 @@
 import { limpiHomeComponents } from '../pages/LimpiHome/consts';
 
-const scroll = (element: string) => {
-  let ele = document.getElementById(element);
-  window.scrollTo({ left: ele?.offsetLeft || 0, top: ele?.offsetTop || 0, behavior: 'smooth' });
+const scroll = (elementId: string) => {
+  let ele = document.getElementById(elementId);
+  ele?.scrollIntoView({ behavior: 'smooth' });
 };
 
 export const jumpTo = (idx: number) => {
